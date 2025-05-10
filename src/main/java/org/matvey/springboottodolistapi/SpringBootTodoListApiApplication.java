fea71190029a465f46967dacdfcbf05495cf6ce5
Matvey <m5221211m@gmail.com>
2025-05-10 16:03:44 +0300

    init

---
+++ src/main/java/org/matvey/springboottodolistapi/SpringBootTodoListApiApplication.java
@@ -0,0 +1,13 @@
+package org.matvey.springboottodolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoListApiApplication.class, args);
+    }
+
+}
